82e733f55e43759869300fe0b5d3677bcfea946f
j4tmr <black4jin@gmail.com>
2024-01-18 10:32:09 +0800

    bug fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
  * @Author: j4tmr black4jin@gmail.com
  * @Date: 2024-01-18 10:05:52
  * @LastEditors: j4tmr black4jin@gmail.com
- * @LastEditTime: 2024-01-18 10:19:49
+ * @LastEditTime: 2024-01-18 10:24:27
  * @FilePath: /ticket/tailwind.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -10,9 +10,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  plugins: [
     require("@tailwindcss/typography"),
     require('@tailwindcss/forms')
   ],
-  plugins: [],
 };
