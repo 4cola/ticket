2ada9cf945c4add8951d8189aa27ee300e94d9e5
j4tmr <black4jin@gmail.com>
2024-01-18 10:24:11 +0800

    init config tailwindcss tsconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,18 @@
+/*
+ * @Author: j4tmr black4jin@gmail.com
+ * @Date: 2024-01-18 10:05:52
+ * @LastEditors: j4tmr black4jin@gmail.com
+ * @LastEditTime: 2024-01-18 10:19:49
+ * @FilePath: /ticket/tailwind.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    require("@tailwindcss/typography"),
+    require('@tailwindcss/forms')
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "hsl(var(--background))",
-        foreground: "hsl(var(--foreground))",
-        btn: {
-          background: "hsl(var(--btn-background))",
-          "background-hover": "hsl(var(--btn-background-hover))",
-        },
-      },
-    },
-  },
   plugins: [],
 };
