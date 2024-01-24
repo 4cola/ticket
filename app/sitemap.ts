db0670289fb209d5dfb7410eb663e913b71eb02f
j4tmr <black4jin@gmail.com>
2024-01-24 14:05:02 +0800

    default bar update frontend layouts

---
+++ app/sitemap.ts
@@ -0,0 +1,73 @@
+/*
+ * @Author: JinBlack
+ * @Date: 2023-10-11 16:31:45
+ * @LastEditors: JinBlack
+ * @LastEditTime: 2024-01-24 14:03:31
+ * @FilePath: /ticket/app/sitemap.ts
+ * @Description: dota2sites@gmail.com
+ *
+ * Copyright (c) 2023 by 4tmr, All Rights Reserved.
+ */
+import { MetadataRoute } from 'next';
+import { createSupaHandler } from '@/libs/supabase';
+
+// export const runtime = 'edge';
+export const revalidate = 7 * 24 * 3600;
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+	const handler = createSupaHandler();
+	const appConfig = await handler.getAppConfig();
+	const { domain } = appConfig.metas;
+	const domainURL = (url: string) => {
+		let u = url;
+		if (!u.startsWith('/')) {
+			u = `/${u}`;
+		}
+		return `https://${domain}${u}`;
+	};
+	let sitemaps: MetadataRoute.Sitemap = appConfig.header.map((navi) => {
+		return {
+			url: domainURL(navi.url),
+			lastModified: new Date()
+		};
+	});
+	// posts
+	const { data: posts } = await handler.client
+		.from('v_posts')
+		.select('updated_at, slug')
+		.eq('is_active', true);
+	sitemaps = [
+		...sitemaps,
+		...(posts
+			?.filter((post) => post.slug !== null)
+			.map((post) => {
+				return {
+					url: domainURL(`/posts/${post.slug!}`),
+					lastModified: post.updated_at!
+				};
+			}) ?? [])
+	];
+	// tags
+	const { data: tags } = await handler.client.from('tags').select('slug');
+	sitemaps = [
+		...sitemaps,
+		...(tags?.map((tag) => {
+			return {
+				url: domainURL(`/posts/tag/${tag.slug}`),
+				lastModified: new Date()
+			};
+		}) ?? [])
+	];
+	// categories
+	const { data: categories } = await handler.client.from('categories').select('slug');
+	sitemaps = [
+		...sitemaps,
+		...(categories?.map((category) => {
+			return {
+				url: domainURL(`/posts/category/${category}`),
+				lastModified: new Date()
+			};
+		}) ?? [])
+	];
+	return sitemaps;
+}
--- components/layouts/Rightbar.tsx
+++ components/layouts/Rightbar.tsx
@@ -2,33 +2,31 @@
  * @Author: JinBlack
  * @Date: 2023-10-11 16:31:45
  * @LastEditors: JinBlack
- * @LastEditTime: 2024-01-24 11:42:16
+ * @LastEditTime: 2024-01-24 13:52:34
  * @FilePath: /ticket/components/layouts/Rightbar.tsx
  * @Description: dota2sites@gmail.com
  *
  * Copyright (c) 2023 by 4tmr, All Rights Reserved.
  */
+'use client';
 import HotTags from '@/components/widgets/HotTags';
 import Categories from '@/components/widgets/Categories';
-import { createSupaHandler } from '@/libs/supabase';
+import { usePathname } from 'next/navigation';
 
-export default async function RightBar() {
-	const handler = createSupaHandler();
-	const appData = await handler.getAppConfig();
-	const { data: categories } = await handler.client
-		.from('v_categories')
-		.select('name,slug')
-		.order('count', { ascending: false })
-		.returns<Category[] | null>();
-	const { data: tags } = await handler.client
-		.from('v_tags')
-		.select('name,slug')
-		.order('count', { ascending: false })
-		.returns<Tag[] | null>();
-	return (
-		<div className="lg:col-span-1 lg:flex hidden lg:flex-col lg:gap-4">
-			<Categories categories={categories} />
-			<HotTags tags={tags} />
-		</div>
-	);
+type Props = {
+	categories: Category[] | null;
+	tags: Tag[] | null;
+};
+
+export default async function RightBar(props: Props) {
+	const pathname = usePathname();
+	if (!pathname.startsWith('/dashboard')) {
+		return (
+			<div className="lg:col-span-1 lg:flex hidden lg:flex-col lg:gap-4">
+				<Categories categories={props.categories} />
+				<HotTags tags={props.tags} />
+			</div>
+		);
+	}
+	return null;
 }
