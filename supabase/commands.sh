521b1d73931dc1f892cba00e9cf23627e93755e5
4tmr <black4jin@gmail.com>
2024-02-01 16:42:55 +0800

    Dev (#43)
    * bug fix
    
    * test upgrade
    
    * bug fix
    
    * update migration
    
    * bug fix
    
    * upgrade database test

--- supabase/commands.sh
+++ supabase/commands.sh
@@ -2,7 +2,7 @@
  # @Author: JinBlack
  # @Date: 2024-01-18 14:49:39
  # @LastEditors: j4tmr black4jin@gmail.com
- # @LastEditTime: 2024-02-01 16:10:14
+ # @LastEditTime: 2024-02-01 16:39:32
  # @FilePath: /ticket/supabase/commands.sh
  # @Description: black4jin@gmail.com
  #
@@ -12,3 +12,4 @@
 supabase gen types typescript --local --schema public > ../types/supabase.ts
 
 supabase migration squash --linked
+supabase migration squash --local
