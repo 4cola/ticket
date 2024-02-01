6d90f0d455ab06914f89c3f35299557eefda6d8e
j4tmr <black4jin@gmail.com>
2024-02-01 17:19:37 +0800

    simple

--- supabase/commands.sh
+++ supabase/commands.sh
@@ -2,7 +2,7 @@
  # @Author: JinBlack
  # @Date: 2024-01-18 14:49:39
  # @LastEditors: j4tmr black4jin@gmail.com
- # @LastEditTime: 2024-02-01 16:39:32
+ # @LastEditTime: 2024-02-01 17:15:52
  # @FilePath: /ticket/supabase/commands.sh
  # @Description: black4jin@gmail.com
  #
@@ -11,5 +11,6 @@
 
 supabase gen types typescript --local --schema public > ../types/supabase.ts
 
+supabase login
 supabase migration squash --linked
 supabase migration squash --local
