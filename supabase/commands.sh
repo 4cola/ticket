7a482514288ba527e3bb549ee52f68c9fa515c3e
j4tmr <black4jin@gmail.com>
2024-03-26 14:19:01 +0800

    update

--- supabase/commands.sh
+++ supabase/commands.sh
@@ -2,7 +2,7 @@
  # @Author: JinBlack
  # @Date: 2024-01-18 14:49:39
  # @LastEditors: JinBlack
- # @LastEditTime: 2024-03-26 14:12:15
+ # @LastEditTime: 2024-03-26 14:14:25
  # @FilePath: /ticket/supabase/commands.sh
  # @Description: black4jin@gmail.com
  #
@@ -12,5 +12,6 @@
 supabase gen types typescript --local --schema public > ../types/supabase.ts
 
 supabase login
+# 下面这两个都不行，得用db-url来解决这个问题，因为旧的机制无法在cli中继续使用！
 supabase migration squash --linked
 supabase migration squash --local
